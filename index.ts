eb40cf96ed7dcc1cc3dbe7d3346e87c42764e937
dlckdduq1107 <dlckdduq97@gmail.com>
2022-03-12 20:19:19 +0900

    함수에서 파라미터 개수도 맞춰줘야 한다.
    JS에서는 파라미터 개수 안 맞춰도 자동으로 순서대로 들어가고 오류가 안난다.

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ function sum(a: number, b: number): number{
     return a+b;
 }
 
-sum(10,20);
+sum(10,20,30,40);//파라미터 개수도 맞아야 한다.
+
 //ts튜플
 let item: [string,number] = ['str',100];
 //길이를 딱 맞춰 줘야함
