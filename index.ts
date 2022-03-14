10eb6393b0160fc5d4028735bac795e8a363b911
dlckdduq1107 <dlckdduq97@gmail.com>
2022-03-14 20:19:19 +0900

    객체 선언법

--- index.ts
+++ index.ts
@@ -7,3 +7,19 @@ sum(10,20,30,40);//파라미터 개수도 맞아야 한다.
 //ts튜플
 let item: [string,number] = ['str',100];
 //길이를 딱 맞춰 줘야함
+
+//ts객체
+let obj: object = {
+    name:'fansor',
+    age:222
+}
+let obj2: {name:string, age:number}={
+    name:'fansor',
+    age:21
+}
+// 옵셔널 파리미터
+function option(a:string, b?:number){
+
+}
+option('asd',10)
+option('asd')
