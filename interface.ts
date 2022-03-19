8537ba29dec711f8e47582ded65e3c9c3a6a3e6d
dlckdduq1107 <dlckdduq97@gmail.com>
2022-03-19 10:12:19 +0900

    interface를 사용해 함수의 구조를 설정해줄수가 있다.

--- interface.ts
+++ interface.ts
@@ -11,3 +11,13 @@ const a:User = {
 const getUser = (user: User)=>{
     console.log(user);
 }
+
+//인터페이스를 가지고 함수의 구조를 설정할 수 도 있다.
+interface sumFunc{
+    (a:number, b:number):number
+}
+let suma: sumFunc;
+
+suma = (a:number, b:number):number=>{
+    return a+b;
+}
